d43d30cd6ea5e4d0142669f229c95d1be89ce2d2
usereim <login3394@gmail.com>
2025-09-05 11:30:56 +0900

    jacat project default setting - 정태양 -

---
+++ Jacat/src/main/java/com/pro/jacat/interceptor/SessionInterceptor.java
@@ -0,0 +1,39 @@
+package com.pro.jacat.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+// http 요청을 가로채는 클래스
+public class SessionInterceptor implements HandlerInterceptor {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("요청 처리 전");
+		
+		// 세션 검사
+//		Object user = request.getSession().getAttribute("user");
+//		
+//		if (user == null) {
+//			response.sendRedirect(request.getContextPath() + "/user/login");
+//			return false;
+//		} 
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("요청 처리 후");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("요청 완료 후");
+	}
+}
