3efc2c2f35fa1dd321bb2671d2afa21d4ac6318e
usereim <login3394@gmail.com>
2025-09-15 12:01:10 +0900

    FileRepository - 정태양

--- Jacat/src/main/java/com/pro/jacat/file/repository/FileRepository.java
+++ Jacat/src/main/java/com/pro/jacat/file/repository/FileRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.pro.jacat.file.vo.BoardsFileVO;
 
+@Repository
 public class FileRepository {
 	
 	private SqlSession template;
@@ -19,7 +21,7 @@ public class FileRepository {
 
 
 	public void insertFileBoardList(List<BoardsFileVO> fileList) {
-		
+		template.insert("fileBoardsMapper.insertFileBoardList", fileList);
 	}
 
 }
