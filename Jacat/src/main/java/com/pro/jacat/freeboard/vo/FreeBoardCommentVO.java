d0f683a2cbf0929bf8e49a2b2a8f0b1f5d97f710
MYCOM <MYCOM@DESKTOP-Q1EEVN9>
2025-09-16 12:24:19 +0900

    freeboard CommentVO 수정

---
+++ Jacat/src/main/java/com/pro/jacat/freeboard/vo/FreeBoardCommentVO.java
@@ -0,0 +1,67 @@
+package com.pro.jacat.freeboard.vo;
+
+import java.sql.Date;
+
+import com.pro.jacat.user.vo.UserVO;
+
+public class FreeBoardCommentVO {
+
+	private int commentNum;
+	private int boardNum;
+	private String usersID;
+	private String content;
+	private Date wDate;
+	private Date modifyDate;
+	private boolean delyn;
+	private UserVO users;
+	
+	public int getCommentNum() {
+		return commentNum;
+	}
+	public void setCommentNum(int commentNum) {
+		this.commentNum = commentNum;
+	}
+	public int getBoardNum() {
+		return boardNum;
+	}
+	public void setBoardNum(int boardNum) {
+		this.boardNum = boardNum;
+	}
+	public String getUsersID() {
+		return usersID;
+	}
+	public void setUsersID(String usersID) {
+		this.usersID = usersID;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getwDate() {
+		return wDate;
+	}
+	public void setwDate(Date wDate) {
+		this.wDate = wDate;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public boolean isDelyn() {
+		return delyn;
+	}
+	public void setDelyn(boolean delyn) {
+		this.delyn = delyn;
+	}
+	public UserVO getUsers() {
+		return users;
+	}
+	public void setUsers(UserVO users) {
+		this.users = users;
+	}
+	
+}
